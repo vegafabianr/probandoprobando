bff73834eb6d39993112d7c7f54f721493839f2f
MMatildeR <114346389+MMatildeR@users.noreply.github.com>
2023-05-01 21:37:34 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,8 @@
+// type="text/javascript"
+ //document.addEventListener('mousemove', function(e){
+   // const body = document.querySelector('body');
+   // const bubbles = document.createElement('span');
+   // bubbles.style.left = e.offsetX+ 'px';
+   // bubbles.style.top = e.offsetY+ 'px';
+   // body.appendChild(bubbles);
+//})
